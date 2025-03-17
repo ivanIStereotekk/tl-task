e9979518659581fe1ec2cefb49b2fd89c25d995a
Ivan Goncharov <ivan.stereotekk@gmail.com>
2025-03-17 08:33:13 +0300

    restructured template

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Django-mptt
 Twitter Bootstrap
 
 
+
+
+
+
--- tl_project/employ_app/views.py
+++ tl_project/employ_app/views.py
@@ -12,4 +12,4 @@ def employee_list(request, department_id):
     department = get_object_or_404(Department, id=department_id)
     employees = department.employees.all()
     data = [{"id": emp.pk, "full_name": emp.full_name, "position": emp.position} for emp in employees]
-    return JsonResponse(employees, safe=False)
+    return JsonResponse(data, safe=False)
